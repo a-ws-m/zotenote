f26ffc9b3ffcfc752a10edfc027c25bb2ad3f300
a-ws-m <amoriarty14@gmail.com>
2021-10-11 19:03:15 +0100

    Update command name

--- src/extension.ts
+++ src/extension.ts
@@ -22,7 +22,7 @@ function formatTemplate(template: string, bibJson: BibJson) {
 
 }
 
-function makeLiteratureNote() {
+function makeLiteratureNotes() {
 	// Show the Zotero citations picker
 	connectToCayw().then(
 		response => {
@@ -49,7 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('zotenote.makeLiteratureNote', makeLiteratureNote);
+	let disposable = vscode.commands.registerCommand('zotenote.makeLiteratureNotes', makeLiteratureNotes);
 
 	context.subscriptions.push(disposable);
 }
